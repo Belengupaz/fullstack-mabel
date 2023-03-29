3e9e0754d3071f781bb8879e0d737de61efe2fe2
Belengupaz <mabelgupaz0111@gmail.com>
2023-03-29 11:58:43 -0400

    Add block 01 to 08

---
+++ block7/Task.js
@@ -0,0 +1,13 @@
+// Create a string for the user
+let messageUser = 'You have received this message because you have been chosen ' +
+'to open an important vault. Here is the secret combination:';
+// Display string in a box dialog
+alert(messageUser);
+// Create variables
+let code1, code2, code3;
+// Assign values to variables
+code1 = 1 + 9;
+code2 = 50 - 10;
+code3 = 13 * 3;
+// Display dialog box with vault codes
+alert(code1 + ' - ' + code2 + ' - ' + code3);
